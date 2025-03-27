eadc50600b9fa262a06e3b35ba095e27bd4cf96f
DavidMichaelson <dovydasbalciunas95@gmail.com>
2025-03-27 17:23:46 +0200

    re-add files, plus js file

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const loginForm = document.getElementById("loginForm");
+    const registerForm = document.getElementById("registerForm");
+    const forgotPasswordForm = document.getElementById("forgotPasswordForm");
+
+    const registerLink = document.getElementById("registerLink");
+    const forgotPasswordLink = document.getElementById("forgotPasswordLink");
+
+    const registerModal = document.getElementById("registerModal");
+    const forgotPasswordModal = document.getElementById("forgotPasswordModal");
+
+    const closeButtons = document.querySelectorAll(".close-btn");
+});
